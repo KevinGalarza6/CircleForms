2d8c9cfa9c7a30c96755fe0db12fe969f12e7b4a
Kevin Galarza <kevin.galarza@focco.com.br>
2021-05-06 20:50:30 -0300

    Add zoom in and zoom out

--- WinForms/Form1.cs
+++ WinForms/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WinForms
@@ -13,10 +7,12 @@ namespace WinForms
     public partial class Form1 : Form
     {
         private Timer _timer;
+        private double _radAngle;
         private double _angle;
         private double _cos;
         private double _sin;
         private double _tan;
+        private float _zoom;
         private Circle _circle;
         private Line _line;
         private Line _xAxis;
@@ -25,10 +21,13 @@ namespace WinForms
         public Form1()
         {
             InitializeComponent();
+            this.DoubleBuffered = true;
             _timer = new Timer();
             _timer.Interval = 16;
             _timer.Tick += _timer_Tick;
             _timer.Start();
+            _zoom = 1;
+            _mousePosition = new PointF { X = 0, Y = 0 };
             _circle = new Circle() { Position = new PointF { X = 0, Y = 0 }, Radius = 200 };
             _line = new Line() { Start = new PointF { X = 0, Y = 0 }, End = new PointF { X = 200, Y = 200 }, Color = Color.Black };
             _xAxis = new Line()
@@ -37,30 +36,35 @@ namespace WinForms
                 End = new PointF { X = this.ClientRectangle.Width, Y = 0 },
                 Color = Color.Blue
             };
-            _yAxis = new Line() {
-                Start = new PointF { X = 0, Y = -this.ClientRectangle.Height }, 
+            _yAxis = new Line()
+            {
+                Start = new PointF { X = 0, Y = -this.ClientRectangle.Height },
                 End = new PointF { X = 0, Y = this.ClientRectangle.Height },
                 Color = Color.Red
             };
+            this.MouseWheel += new MouseEventHandler(this.panel1_MouseWheel);
         }
 
         private void _timer_Tick(object sender, EventArgs e)
         {
-            _angle += 0.01;
+            _radAngle += 0.01;
+            _angle = _radAngle * (180 / Math.PI);
 
-            if (_angle >= Math.PI * 2)
-                _angle -= Math.PI * 2;
+            if (_radAngle >= Math.PI * 2)
+                _radAngle -= Math.PI * 2;
 
-            _cos = Math.Cos(_angle);
-            _sin = Math.Sin(_angle);
-            _tan = Math.Tan(_angle);
+            _cos = Math.Cos(_radAngle);
+            _sin = Math.Sin(_radAngle);
+            _tan = Math.Tan(_radAngle);
             _line.End = new PointF() { X = (float)(_cos * _circle.Radius), Y = (float)(_sin * _circle.Radius) };
+
             this.Invalidate();
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             e.Graphics.TranslateTransform(this.ClientRectangle.Width / 2, this.ClientRectangle.Height / 2);
+            e.Graphics.ScaleTransform(_zoom, _zoom);
             _xAxis.Draw(e.Graphics);
             _yAxis.Draw(e.Graphics);
             DrawValues(e.Graphics);
@@ -72,12 +76,19 @@ namespace WinForms
         {
             string format = $"angle = {_angle:0.###} | cos = {_cos:0.###} | sen = {_sin:0.###} | tan = {_tan:0.###}";
 
-            g.DrawString(format, this.Font, System.Drawing.Brushes.Red, _line.End);
+            g.DrawString(format, this.Font, Brushes.Red, _line.End);
         }
 
-        private void Clear(PaintEventArgs e)
+        private void panel1_MouseWheel(object sender, MouseEventArgs e)
         {
-            e.Graphics.Clear(Color.White);
+            if (e.Delta > 0 && _zoom + 1 != 0)
+            {
+                _zoom *= (float)1.25;
+            }
+            else if (e.Delta < 0 && _zoom - 1 != 0)
+            {
+                _zoom /= (float)1.25;
+            }
         }
     }
 }
